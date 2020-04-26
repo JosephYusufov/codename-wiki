0e22c112c2123ae2a9c530496b66cbb8883f79f8
Joseph Yusufov <jyusufov00@stuy.edu>
2020-04-25 22:36:45 -0400

    added fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-
 import React from 'react';
+import './styles/App.scss';
 
 export default function App(){
   return(
     <div>
       <h1>Hello World</h1>
+      <div className="columns">
+        <div className="column">1</div>
+        <div className="column">2</div>
+        <div className="column">3</div>
+        <div className="column">4</div>
+        <div className="column">5</div>
+      </div>
     </div>
   );
 }
